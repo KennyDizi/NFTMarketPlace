feb11804c65b116a9904b6803751d50aa1e4102b
KennyDizi <xamarindevelopervietnam@outlook.com>
2022-01-17 21:25:29 +0700

    add token configuration file

---
+++ contracts/KDZTokenConfig.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.11;
+
+// ----------------------------------------------------------------------------
+// KDZTokenConfig - KDZ Token Configuration
+//
+// Copyright (c) 2022 Kenny Dizi
+//
+// ----------------------------------------------------------------------------
+
+contract KDZTokenConfig {
+    string public constant TOKEN_NAME = "KDZ NFT"; // Place your own token name
+    string public constant TOKEN_SYMBOL = "KDZ";
+    string public constant KDZ_MARKETPLACE_NAME = "KDZ NFT MARKETPLACE";
+}
